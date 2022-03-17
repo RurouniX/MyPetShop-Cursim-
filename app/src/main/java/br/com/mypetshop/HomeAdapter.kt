6e3222201ebc3f59f444c8d1f419beaa59b6d8d3
Marcos Castro <vincastro@gmail.com>
2022-03-17 17:26:55 -0300

    Solução do segundo desafio

--- app/src/main/java/br/com/mypetshop/HomeAdapter.kt
+++ app/src/main/java/br/com/mypetshop/HomeAdapter.kt
@@ -1,23 +1,27 @@
 package br.com.mypetshop
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class HomeAdapter(private val itemList: List<String>) : RecyclerView.Adapter<HomeViewHolder>() {
+class HomeAdapter(private val itemList: List<ItemListModel>, private val callback : ()-> Unit) :
+    RecyclerView.Adapter<HomeViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
 
-        return HomeViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.adapter_home, parent, false)
-        )
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.adapter_home, parent, false)
+        return HomeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        holder.bind(itemList[position])
+        holder.bind(itemList, position, callback)
     }
 
     override fun getItemCount(): Int {
@@ -27,10 +31,52 @@ class HomeAdapter(private val itemList: List<String>) : RecyclerView.Adapter<Hom
 
 class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(item: String) {
+    private val title = itemView.findViewById<TextView>(R.id.home_adapter_title)
+    private val subtitle = itemView.findViewById<TextView>(R.id.home_adapter_subtitle)
+    private val check = itemView.findViewById<CheckBox>(R.id.home_adapter_check)
 
-        Log.d("ItemAdapter", item)
-        itemView.findViewById<TextView>(R.id.adapter_text).text = item
+    fun bind(itemList: List<ItemListModel>, position: Int, callback : ()-> Unit) {
 
+        val item = itemList[position]
+
+        title.text = item.title
+        subtitle.text = item.subtitle
+
+        with(check) {
+            isChecked = item.isActive
+
+            setOnClickListener {
+
+                itemList.forEach {
+                    it.isActive = false
+                }
+
+                item.isActive = true
+
+                callback()
+
+            }
+        }
+
+        with(itemView) {
+            setOnClickListener {
+
+
+                check.isChecked = !check.isChecked
+
+
+                Toast.makeText(
+                    itemView.context,
+                    "Título - ${item.title} --- Subtitulo - ${item.subtitle} --- IsChecked - ${item.isActive}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            if (position % 2 == 0)
+                setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.yellow))
+            else
+                setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.blue))
+
+        }
     }
 }
